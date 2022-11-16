1fd27b71457397c00fa00420f01d521be8e85bb1
bv-ankit <ankit@blogvault.net>
2022-11-16 13:40:36 +0530

    Added http retries and process_alert_job logger

--- app/models/request_module.rb
+++ app/models/request_module.rb
@@ -1,8 +1,13 @@
 module RequestModule
+	MAX_RETIES = 2
 	def self.send_request(url, params)
+		tries ||= 1
 		headers = { 'Accept' => 'application/json' }
 		HTTParty.get(url, :headers => headers, :query => params)
 	rescue => e
-		# retry
+		logger.error "HTTP_REQUEST_ERROR: #{e.class} - #{e.message} - #{e.backtrace}"
+		tries += 1
+		retry if tries < MAX_RETIES
+		raise e
 	end
 end
--- app/sidekiq/process_alert_job.rb
+++ app/sidekiq/process_alert_job.rb
@@ -1,5 +1,5 @@
 class ProcessAlertJob
-  include Sidekiq::Job
+	include Sidekiq::Job
 
 	def perform
 		# TODO: redis locks to prevent same job parallel execution
@@ -7,9 +7,15 @@ class ProcessAlertJob
 		actions.pluck(:monitoring_query_id).uniq.each { |query_id|
 			ALERT_EMAIL_IDS.each { |email|
 				AlertMailer.with(:query_id => query_id, :email => email).new_entries_alert.deliver_now #async required?
-				# what if it fails partially?
+				# TODO: Store sent alert logs in a table
 			}
 		}
 		actions.update_all(:notified => true)
+	rescue => e
+		logger.error "PROCESS_ALERT_JOB_FAILED: #{e.class} - #{e.message} - #{e.backtrace}"
+		#TODO: stat
+		#TODO: Admin alert
+	ensure
+		ProcessAlertJob.perform_in(1.minutes)
 	end
 end
